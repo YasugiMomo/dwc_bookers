5ed5bebd34ab7dff7f82acdc0699b66f8e44e2cd
EC2 Default User <ec2-user@ip-172-31-18-54.ec2.internal>
2022-03-09 18:28:35 +0000

    [Update]3-12 createの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,20 @@
 class BooksController < ApplicationController
+
+
   def index
+    @books = Book.all
     @book = Book.new
-    
+
   end
-  
+
   def create
     # データを受け取り新規登録するためのインスタンス作成
-    book = book.new(book_params)
+    @book = Book.new(book_params)
     # データをデータベースに保存するためのsaveメソッド実行
-    book.save
+    @book.save
     # showの画面へリダイレクト
-    redirect_to '/books'
-    
+    redirect_to 'books#show'
+
   end
 
   def show
@@ -21,6 +24,6 @@ class BooksController < ApplicationController
   end
   # ストロングパラメータ
   def book_params
-    params.require(:list).permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
